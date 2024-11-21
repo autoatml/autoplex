4f27326f5ef93ce7053b85c562ae8a6690d145be
QuantumChemist <c.zitlau@live.com>
2024-11-21 14:54:00 +0100

    fix docstrings

--- src/autoplex/data/rss/jobs.py
+++ src/autoplex/data/rss/jobs.py
@@ -440,7 +440,7 @@ def do_rss_single_node(
     ----------
     mlip_type: str
         Choose one specific MLIP type:
-        'GAP' | 'J-ACE' | 'P-ACE' | 'NequIP' | 'M3GNet' | 'MACE'.
+        'GAP' | 'J-ACE' | 'NequIP' | 'M3GNet' | 'MACE'.
     mlip_path: str
         Path to the MLIP model.
     iteration_index: str
