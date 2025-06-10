b9f99eb0b74eb5212cbe68ed6e4cd12d3d70ea09
JaGeo <janinegeorge.ulfen@gmail.com>
2025-06-10 21:10:43 +0200

    fix passing all arguments

--- src/autoplex/data/common/jobs.py
+++ src/autoplex/data/common/jobs.py
@@ -612,6 +612,7 @@ def collect_dft_data(
             elif has_ase_output:
                 at = read(os.path.join(val, "final_atoms_object.xyz"), index=":")
             for at_i in at:
+                print(at_i.get_stress())
                 virial_list = (
                     -voigt_6_to_full_3x3_stress(at_i.get_stress()) * at_i.get_volume()
                 )
