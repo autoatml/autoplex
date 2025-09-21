f35aa7ad66ec563740eebca88072a483cabd1460
YuanbinLiu <lyb122502@126.com>
2025-09-21 03:06:21 +0100

    add unit tests

--- tests/castep/test_utils.py
+++ tests/castep/test_utils.py
@@ -1,68 +1,43 @@
-"""
-Integration test for CASTEP through DFTlabelling workflow.
-Tests that BaseCastepMaker can successfully run a CASTEP calculation.
-"""
-
-import os
-os.environ["CASTEP_COMMAND"] = "/usr/local/CASTEP-20/castep.mpi"
-os.environ["CASTEP_PP_PATH"] = "/usr/local/CASTEP-20/usp"
-import datetime
+import gzip
+from pathlib import Path
 from ase import Atoms
-from pymatgen.io.ase import AseAtomsAdaptor
-from jobflow import run_locally, Flow
-from autoplex.castep.jobs import BaseCastepMaker
 from ase.calculators.castep import Castep
-from autoplex.castep.utils import CastepInputGenerator
-import shutil
-import glob
+from pymatgen.io.ase import AseAtomsAdaptor
+from autoplex.castep.utils import CastepStaticSetGenerator, gzip_castep_outputs
 
 
-def test_BaseCastepMaker(memory_jobstore):
-    
-    atoms = Atoms('Si', positions=[[10.0, 10.0, 10.0]], cell=[20, 20, 20], pbc=True)
-    calc = Castep()
-    calc.param.cut_off_energy = 100.0
-    calc.param.xc_functional = 'PBE'
-    calc.param.task = 'SinglePoint'
-    calc.param.max_scf_cycles = 100
-    calc.param.elec_energy_tol = 1e-6
-    calc.cell.kpoint_mp_grid = '1 1 1'
-    calc.cell.kpoint_mp_offset = '0.0 0.0 0.0'
-    calc.param.spin_polarized = False
-    
-    atoms.calc = calc
-    ase_energy = atoms.get_potential_energy()
-    print('ase_energy:', ase_energy)
-    
-    pmg_structure = AseAtomsAdaptor.get_structure(atoms)
-    
-    castep_job = BaseCastepMaker(
-        name="test_castep",
-        input_set_generator=CastepInputGenerator(
-            user_param_settings={
-            'cut_off_energy': 100.0,
-            'xc_functional': 'PBE',
-            'task': 'SinglePoint',
-            'max_scf_cycles': 100,
-            },
-            user_cell_settings={
-            'kpoint_mp_grid': '1 1 1',
-            'kpoint_mp_offset': '0.0 0.0 0.0',
-            }
-        )
-    ).make(structure=pmg_structure)
-    
-    job_rss = Flow(castep_job, output=castep_job.output)
-    run_locally(job_rss,
-                ensure_success=True,
-                create_folders=True,
-                store=memory_jobstore)
-    
-    dict_castep = castep_job.output.resolve(memory_jobstore)
-    
-    assert abs(ase_energy - dict_castep["energy"]) < 1e-4
-    
-    for d in glob.glob("job_*") + glob.glob("CASTEP*"):
-        shutil.rmtree(d, ignore_errors=True)
-    
-    
+def test_CastepStaticSetGenerator():
+    atoms = Atoms("Si", positions=[[0, 0, 0]], cell=[5, 5, 5], pbc=True)
+    structure = AseAtomsAdaptor.get_structure(atoms)
+
+    gen = CastepStaticSetGenerator(
+        user_param_settings={"cut_off_energy": 100.0, "xc_functional": "PBE"},
+        user_cell_settings={"kpoint_mp_grid": "1 1 1"},
+    )
+
+    input_set = gen.get_input_set(structure=structure)
+
+    assert input_set["param"]["cut_off_energy"] == 100.0
+    assert input_set["param"]["xc_functional"] == "PBE"
+    assert input_set["param"]["task"] == "SinglePoint"
+
+    assert "kpoint_mp_grid" in input_set["cell"]
+    assert "kpoints_mp_spacing" in input_set["cell"]
+
+    assert input_set["structure"].composition.formula == "Si1"
+
+
+def test_gzip_castep_outputs(tmp_path):
+    test_file = tmp_path / "test.castep"
+    with open(test_file, "w") as f:
+        f.write("dummy CASTEP output")
+
+    gzip_castep_outputs(workdir=tmp_path)
+
+    gz_path = tmp_path / "test.castep.gz"
+
+    assert gz_path.exists()
+
+    with gzip.open(gz_path, "rt") as f:
+        content = f.read()
+    assert "dummy CASTEP output" in content
