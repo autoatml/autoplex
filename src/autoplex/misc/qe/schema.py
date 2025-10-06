a0e61590844ef0bd6fc14edc5d42ee867dc5f6dd
41bY <alberto.pacini2@studio.unibo.it>
2025-10-06 08:52:39 +0200

    Consistent format for QE schema

--- src/autoplex/misc/qe/schema.py
+++ src/autoplex/misc/qe/schema.py
@@ -2,6 +2,10 @@ from __future__ import annotations
 
 from typing import Dict, List, Optional
 
+from pymatgen.core import Structure
+from emmet.core.structure import StructureMetadata
+from emmet.core.math import Matrix3D, Vector3D
+
 from pydantic import BaseModel, Field, field_validator
 
 
@@ -37,21 +41,61 @@ class QeKpointsSettings(BaseModel):
         return v
 
 
-class QeInputSet(BaseModel):
+class InputDoc(BaseModel):
     """
-    Files and contexts for static SCF job
+    Inputs and contexts used to run the static SCF job
     """
     workdir: str
     pwi_path: str
     seed: str
+    run_settings: QeRunSettings = Field(None, description="QE namelist section with: &control, &system, &electrons")
+    kpoints: QeKpointsSettings = Field(None, description="QE K_POINTS settings")
 
 
-class QeRunResult(BaseModel):
+class OutputDoc(BaseModel):
     """
-    Results of the SCF job
+    The outputs of this jobs
     """
-    success: bool
-    pwi: str
-    pwo: str
-    outdir: str
-    total_energy_ev: Optional[float] = None
+    energy: float | None = Field(None, description="Total energy in units of eV.")
+
+    energy_per_atom: float | None = Field(
+        None,
+        description="Energy per atom of the final molecule or structure "
+        "in units of eV/atom.",
+    )
+
+    forces: list[Vector3D] | None = Field(
+        None,
+        description=(
+            "The force on each atom in units of eV/A for the final molecule "
+            "or structure."
+        ),
+    )
+
+    # NOTE: units for stresses were converted to kbar (* -10 from standard output)
+    #       to comply with MP convention
+    stress: Matrix3D | None = Field(
+        None, description="The stress on the cell in units of kbar."
+    )
+
+class TaskDoc(StructureMetadata):
+    """Document containing information on structure manipulation using Quantum ESPRESSO."""
+
+    structure: Structure = Field(
+        None, description="Final output structure from the task"
+    )
+
+    input: InputDoc = Field(
+        None, description="The input information used to run this job."
+    )
+
+    output: OutputDoc = Field(None, description="The output information from this job.")
+
+    task_label: str = Field(
+        None,
+        description="Description of the CASTEP task (e.g., static, relax)",
+    )
+
+    dir_name: str | None = Field(
+        None, description="Directory where the QE calculations are performed."
+    )    
