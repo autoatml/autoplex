ffca2d41fac6e0d87b5e710cc3adb8af45c3411a
Andrei Sobolev <sobolev@ms1p.org>
2026-02-20 17:00:21 +0100

    Changed tests to use atomate2 utils

--- tests/misc/fhi-aims/test_aims.py
+++ tests/misc/fhi-aims/test_aims.py
@@ -1,3 +1,4 @@
+import pytest
 from ase.build import bulk
 from ase.io import read
 from jobflow import run_locally, Flow
@@ -8,13 +9,12 @@ from autoplex.data.common.jobs import collect_dft_data
 from pymatgen.io.ase import AseAtomsAdaptor
 
 
-def test_dft_labelling_with_aims(memory_jobstore, mock_aims, clean_dir):
+def test_dft_labelling_with_aims(memory_jobstore, mock_aims, test_dir):
     """Test DFTStaticLabelling job with FHi-aims static maker."""
     ref_paths = {
         "static_bulk_0": "bulk_0",
         "static_bulk_1": "bulk_1",
     }
-
     mock_aims(ref_paths)
 
     atoms = [bulk("Si", "diamond", a=a) for a in (5.1, 5.2)]
@@ -24,7 +24,7 @@ def test_dft_labelling_with_aims(memory_jobstore, mock_aims, clean_dir):
         name="static_aims",
         input_set_generator=AimsStaticSetGenerator(
             user_params={
-                "species_dir": "light",
+                "species_dir": f"{test_dir}/fhi-aims/species_defaults",
                 "xc": "pbe",
                 "compute_forces": True,
                 "compute_analytical_stress": True,
@@ -53,6 +53,5 @@ def test_dft_labelling_with_aims(memory_jobstore, mock_aims, clean_dir):
     path_to_aims = dict_dft['dft_ref_dir']
 
     atoms = read(path_to_aims, index=":")
-    config_types = [at.info['config_type'] for at in atoms]
-
-    assert len(config_types) == 2
+    energies = [at.info['REF_energy'] for at in atoms]
+    assert all([e == pytest.approx(-15802, abs=1) for e in energies])
