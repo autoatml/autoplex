27b09978cd3b629d1211e3fe3b7e9ee9727f2a81
Hanna Tuerk <hanna.tuerk@bam.de>
2026-08-11 10:24:23 +0200

    fix fragment rss test

--- tests/data/rss/test_jobs.py
+++ tests/data/rss/test_jobs.py
@@ -495,7 +495,7 @@ def test_fragment_buildcell_multifrags(test_dir, memory_jobstore, clean_dir):
     ])
     frags=[Li, PS4]
 
-    frag[0].write(f'{test_dir}/data/fragments.extxyz', )
+    frags[0].write(f'{test_dir}/data/fragments.extxyz', )
     for frag in frags[1:]:
         frag.write(f'{test_dir}/data/fragments.extxyz', append=True)
     
